2e7c49a36080ea12b46ca60983e79c2a6d9299c2
willy <willybalt.2017@gmail.com>
2026-03-04 16:30:27 -0400

    configuracion de .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// Habilita el uso de .env 
+import { ConfigModule } from '@nestjs/config';
+
+// TypeORM para la conexion con la base de datos (MySQL)
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
