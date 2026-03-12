f7c8c0506934db104ee4421aa682d4c8497eb485
willy <willybalt.2017@gmail.com>
2026-03-12 18:42:59 -0400

    task.entity.ts

--- src/modules/task/entities/task.entity.ts
+++ src/modules/task/entities/task.entity.ts
@@ -1,1 +1,17 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Task {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+}
