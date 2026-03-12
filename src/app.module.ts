c0b54d1eb961b21d59f0b8fad77fa5271e814440
willy <willybalt.2017@gmail.com>
2026-03-12 18:37:54 -0400

    instalacion de typeORM y conexion con mySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,24 @@ import { ConfigModule } from '@nestjs/config';
 
 // TypeORM para la conexion con la base de datos (MySQL)
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskModule } from './modules/task/task.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'task',
+      entities: [],
+      synchronize: true,
+    }),
+    TaskModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
