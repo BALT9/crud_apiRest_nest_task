d3a6bc0e0130ef56d1e5085afdb036ae9994beeb
willy <willybalt.2017@gmail.com>
2026-03-12 18:32:37 -0400

    endpoint saludo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,10 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/saludo')
+  getSaludo() : string{
+    return this.appService.getSaludo();
+  }
+
 }
