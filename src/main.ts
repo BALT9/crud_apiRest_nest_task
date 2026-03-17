8ed44a854d09774b75c0120ce23555e9f5ffda37
willy <willybalt.2017@gmail.com>
2026-03-17 17:59:21 -0400

    Habilitacion de Cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilitacion de Cors 
+  app.enableCors();
+
+  // Swagger 
   const config = new DocumentBuilder()
     .setTitle('Crus Task Nest')
     .setDescription('The Task API description')
@@ -13,7 +17,10 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, documentFactory);
+
+  // Validacion de Dto 
   app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
