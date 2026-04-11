ddff330fe735547c16cfd16eab4527cb3337a955
willy <willybalt.2017@gmail.com>
2026-04-11 15:44:07 -0400

    ajuste deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,12 +16,11 @@ import { UserModule } from './modules/user/user.module';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +`${process.env.DATABASE_PORT}`,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'], 
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
     }),
     TaskModule,
